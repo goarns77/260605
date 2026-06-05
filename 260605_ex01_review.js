3dfe6f4eee7eb449da3fa5cfb23505360e98cae5
goarns77 <goarns771@gmail.com>
2026-06-05 10:27:19 +0900

    feat: initialize Express server project with dotenv and nodemon configuration

---
+++ 260605_ex01_review.js
@@ -0,0 +1,16 @@
+//환경 변수
+const dotenv = require("dotenv");
+dotenv.config();
+// == require("dotenv").config();
+
+//의존성
+const express = require("express");
+const PORT = process.env.PORT ?? 3000;
+
+//서버 세팅
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
